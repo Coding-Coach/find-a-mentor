f7a8b7db037c6e809d8cebd7877bb01f07b841c7
Mosh Feu <moshfeu@gmail.com>
2019-02-26 00:19:35 +0200

    try to commit from travis

--- scripts/sync-lists.js
+++ scripts/sync-lists.js
@@ -1,4 +1,5 @@
 const { writeFileSync } = require('fs');
+const cp = require('child_process');
 
 function generateLists() {
   const mentors = require('../src/mentors.json');
@@ -8,13 +9,28 @@ function generateLists() {
   }
 
   for (let i = 0; i < mentors.length; i++) {
-    json.tags.push(...mentors[i].tags);
+    json.tags.push(...(mentors[i].tags || []));
     json.countries.push(mentors[i].country);
   }
+
+  json.tags = [...new Set(json.tags)]
+  json.countries = [...new Set(json.countries)]
 
   return json;
 }
 
 writeFileSync('src/lists.json', JSON.stringify(generateLists()));
+const command = `
+if [[ ! -z $(git status -s) ]]
+then
+  git add src/lists.json
+  git commit -m "[auto] update lists"
+  if [ "\${TRAVIS_PULL_REQUEST}" = "true" ]
+  then
+    git push
+  fi
+fi
+`;
+cp.execSync(command);
 
 module.exports = generateLists;
