5e9d4c13961f356a09ea90193fcc6d1516f5c5ca
Prateek Gogia <gogia.prateek@hotmail.com>
2019-03-10 19:16:57 +0530

    Fixed responsiveness on bigger screens

--- src/components/MentorsList/MentorsList.js
+++ src/components/MentorsList/MentorsList.js
@@ -10,75 +10,90 @@ import { toggle, get } from '../../favoriteManager';
 
 const itemsInPage = 10;
 
-const tagsList = tags => tags.map((tag, index) => {
-  return (
-    <Label className="mentor-tag" key={index}>{tag}</Label>
-  );
-});
-
-const items = (mentors, favs, onToggleFav) => mentors.map((mentor, index) => {
-  return (<Card className="mentor-card" key={index}>
-    <header>
-      <Label as='button' corner="right" onClick={onToggleFav.bind(null, mentor)}>
-        <Icon name="heart" color={favs.indexOf(mentor.id) > -1 ? 'red' : 'black'} />
+const tagsList = tags =>
+  tags.map((tag, index) => {
+    return (
+      <Label className="mentor-tag" key={index}>
+        {tag}
       </Label>
-      <img src={mentor.avatar} alt={`${mentor.name}'s avatar`} />
-      <div className="details">
-        <Card.Header>{mentor.name}</Card.Header>
-        <Card.Meta>{mentor.title}</Card.Meta>
-        <div className="tags">
-          {tagsList(mentor.tags)}
+    );
+  });
+
+const items = (mentors, favs, onToggleFav) =>
+  mentors.map((mentor, index) => {
+    return (
+      <Card className="mentor-card" key={index}>
+        <header>
+          <Label
+            as="button"
+            corner="right"
+            onClick={onToggleFav.bind(null, mentor)}
+          >
+            <Icon
+              name="heart"
+              color={favs.indexOf(mentor.id) > -1 ? 'red' : 'black'}
+            />
+          </Label>
+          <img src={mentor.avatar} alt={`${mentor.name}'s avatar`} />
+          <div className="details">
+            <Card.Header>{mentor.name}</Card.Header>
+            <Card.Meta>{mentor.title}</Card.Meta>
+            <div className="tags">{tagsList(mentor.tags)}</div>
+          </div>
+        </header>
+        <div className="details">
+          <Card.Description>
+            <Icon className="mentor-quote" name="quote left" />
+            {mentor.description}
+          </Card.Description>
         </div>
-      </div>
-    </header>
-    <div className="details">
-      <Card.Description>
-        <Icon className="mentor-quote" name="quote left" />
-        {mentor.description}
-      </Card.Description>
-    </div>
-    <Button.Group attached='bottom'>
-    {
-      mentor.channels.map(channel => {
-        const { icon, url } = getChannelInfo(channel);
-        return (
-          <a className="ui black basic button channel-link" key={channel.type} href={url} target="_blank" rel="noopener noreferrer">
-            <Icon name={icon} />
-            <span>{channel.type}</span>
-          </a>
-        )
-      })
-    }
-    </Button.Group>
-  </Card>)
-});
+        <Button.Group attached="bottom">
+          {mentor.channels.map(channel => {
+            const { icon, url } = getChannelInfo(channel);
+            return (
+              <a
+                className="ui black basic button channel-link"
+                key={channel.type}
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon name={icon} />
+                <span>{channel.type}</span>
+              </a>
+            );
+          })}
+        </Button.Group>
+      </Card>
+    );
+  });
 
 export default class MentorsLists extends Component {
   state = {
     page: 1,
     favs: get()
-  }
+  };
 
   loadMore = () => {
     this.setState({
       page: this.state.page + 1
-    })
-  }
+    });
+  };
 
   componentWillReceiveProps(newProps) {
     if (newProps.mentors !== this.props.mentors) {
       this.setState({
         page: 1
-      })
+      });
     }
   }
 
-  onToggleFav = (mentor) => {
+  onToggleFav = mentor => {
     const favs = toggle(mentor);
     this.setState({
       favs
-    })
-  }
+    });
+  };
 
   render() {
     const { mentors, className } = this.props;
@@ -87,10 +102,7 @@ export default class MentorsLists extends Component {
     const mentorsInList = mentors.slice(0, page * itemsInPage);
 
     return (
-      <div className={classNames([
-        'mentors-wrapper',
-        className
-      ])}>
+      <div className={classNames(['mentors-wrapper', className])}>
         <Card.Group centered className="mentors-cards">
           <InfiniteScroll
             className="ui centered cards mentors-cards"
@@ -100,6 +112,7 @@ export default class MentorsLists extends Component {
             {items(mentorsInList, favs, this.onToggleFav)}
           </InfiniteScroll>
         </Card.Group>
-      </div>);
+      </div>
+    );
   }
 }
