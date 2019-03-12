b74cdce9b10cabc04d809f6a097bbb31d09de233
Mosh Feu <moshfeu@gmail.com>
2019-03-12 09:40:30 +0200

    resolve #55 - Restrict avatar image URL to https in tests

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Just go to https://mentors.codingcoach.io/ and find her / him.
 {
   "id": "your email",
   "name": "your name",                          // minLength: 2
-  "avatar": "https://link-to-your/avatar.jpg",  // url
+  "avatar": "https://link-to-your/avatar.jpg",  // url, must start with https://
   "title": "NodeJS developer",                  // minLength: 2, maxLength: 30
   "description": "Hi, I'm NodeJs developer",    // minLength: 5, maxLength: 80 optional
   "country": "Israel",                          // Full name. please avoid synonyms (check if it's not already exist)
--- src/__tests__/mentors.json-test.js
+++ src/__tests__/mentors.json-test.js
@@ -4,71 +4,92 @@ import lists from '../lists.json';
 
 import generateLists from '../../scripts/generate-lists';
 
-it('should mentors json contains all fields', () => {
-  var ajv = new Ajv({ removeAdditional: false });
-  const valid = ajv.validate({
-    "type": "array",
-    "items": {
-      "type": "object",
-      "properties": {
-        "id": {
+expect.extend({
+  toBeValid(isValid, errorMessage) {
+    return {
+      message: () => isValid ? '' : errorMessage,
+      pass: isValid
+    }
+  }
+});
+
+var ajv = new Ajv({ removeAdditional: false });
+
+const validate = function (schema, uri) {
+  validate.errors = [{keyword: 'secured', message: 'avatar url must be "https" schema', params: {keyword: 'secured'}}];
+  return uri.indexOf('https://') === 0;
+};
+
+ajv.addKeyword('securedUrl', {
+  validate,
+  errors: true
+});
+
+const mentorSchema = {
+  "type": "array",
+  "items": {
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "string",
+        "format": "email"
+      },
+      "name": {
+        "type": "string",
+        "minLength": 2
+      },
+      "avatar": {
+        "type": "string",
+        "format": "uri",
+        "securedUrl": true,
+      },
+      "title": {
+        "type": "string",
+        "minLength": 2,
+        "maxLength": 35
+      },
+      "description": {
+        "type": "string",
+        "minLength": 5,
+        "maxLength": 80
+      },
+      "country": {
+        "type": "string",
+      },
+      "tags": {
+        "type": "array",
+        "minItems": 1,
+        "maxItems": 5,
+        "items": {
           "type": "string",
-          "format": "email"
-        },
-        "name": {
-          "type": "string",
-          "minLength": 2
-        },
-        "avatar": {
-          "type": "string",
-          "format": "uri"
-        },
-        "title": {
-          "type": "string",
-          "minLength": 2,
-          "maxLength": 35
-        },
-        "description": {
-          "type": "string",
-          "minLength": 5,
-          "maxLength": 80
-        },
-        "country": {
-          "type": "string",
-        },
-        "tags": {
-          "type": "array",
-          "minItems": 1,
-          "maxItems": 5,
-          "items": {
-            "type": "string",
-            "minLength": 1,
-            "maxLength": 15
-          }
-        },
-        "channels": {
-          "type": "array",
-          "minItems": 1,
-          "maxItems": 3,
-          "items": {
-            "type": "object",
-            "properties": {
-              "type": {
-                "type": "string",
-                "enum": ["slack", "email", "linkedin", "facebook", "twitter"]
-              }
-            },
-            "required": ["type", "id"]
-          }
+          "minLength": 1,
+          "maxLength": 15
         }
       },
-      "required": ["id", "name", "avatar", "title", "country", "tags", "channels"]
-    }
-  }, mentors);
-  if (ajv.errors) {
-    console.log(ajv.errors);
+      "channels": {
+        "type": "array",
+        "minItems": 1,
+        "maxItems": 3,
+        "items": {
+          "type": "object",
+          "properties": {
+            "type": {
+              "type": "string",
+              "enum": ["slack", "email", "linkedin", "facebook", "twitter"]
+            }
+          },
+          "required": ["type", "id"]
+        }
+      }
+    },
+    "required": ["id", "name", "avatar", "title", "country", "tags", "channels"]
   }
-  expect(valid).toBeTruthy();
+}
+
+it('should mentors shema should be valid', () => {
+  const valid = ajv.validate(mentorSchema, mentors);
+  const errorMessage = (ajv.errors || []).map(error => error.message).join('\n');
+  expect(valid).toBeValid(errorMessage);
 });
 
 it('should lists be synced with the mentors details', () => {
