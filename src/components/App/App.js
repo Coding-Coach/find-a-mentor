ab4917c940cfe4c6af5874460e41a0fb2a31e3bb
Emma Wedekind <emma.wedekind@logmein.com>
2019-03-14 16:43:51 +0100

    Changing header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import mentors from '../../mentors.json';
+import "./App.css";
+import mentors from "../../mentors.json";
 
-import React, { Component } from 'react';
-import { Header, Button, Icon } from 'semantic-ui-react';
-import classNames from 'classnames';
-import AutoComplete from '../AutoComplete/AutoComplete';
-import MentorsList from '../MentorsList/MentorsList';
-import Logo from '../Logo';
-import shuffle from 'lodash/shuffle';
-import { generateLists } from '../../listsGenerator';
+import React, { Component } from "react";
+import { Header, Button, Icon } from "semantic-ui-react";
+import classNames from "classnames";
+import AutoComplete from "../AutoComplete/AutoComplete";
+import SocialLinks from "../SocialLinks";
+import MentorsList from "../MentorsList/MentorsList";
+import Logo from "../Logo";
+import shuffle from "lodash/shuffle";
+import { generateLists } from "../../listsGenerator";
 
 const { tags, countries } = generateLists(mentors);
-const mapToDropdown = item => ({title: item})
+const mapToDropdown = item => ({ title: item });
 const tagsSource = tags.map(mapToDropdown);
 const countriesSource = countries.map(mapToDropdown);
 
@@ -19,50 +20,45 @@ const countriesSource = countries.map(mapToDropdown);
 class App extends Component {
   state = {
     mentors: shuffle(mentors)
-  }
+  };
 
-  handleTagSelect = (result) => {
+  handleTagSelect = result => {
     this.setState({
       tag: result.title
-    })
-  }
+    });
+  };
 
-  handleCountrySelect = (result) => {
+  handleCountrySelect = result => {
     this.setState({
       country: result.title
-    })
-  }
+    });
+  };
 
-  filterMentors = mentor =>  {
+  filterMentors = mentor => {
     const { tag, country } = this.state;
-    return (!tag || mentor.tags.includes(tag)) &&
-           (!country || mentor.country === country);
-  }
+    return (
+      (!tag || mentor.tags.includes(tag)) &&
+      (!country || mentor.country === country)
+    );
+  };
 
   resetTag = () => {
     this.setState({
-      tag: '',
+      tag: ""
     });
-  }
+  };
 
   resetCountry = () => {
     this.setState({
-      country: ''
+      country: ""
     });
-  }
+  };
 
   toggleFields = () => {
     this.setState({
       fieldsIsActive: !this.state.fieldsIsActive
-    })
-  }
-
-  // async componentDidMount() {
-  //   const mentors = await fetch(`${serverEndpoint}/get_mentors`).then(res => res.json());
-  //   this.setState({
-  //     mentors
-  //   })
-  // }
+    });
+  };
 
   render() {
     const { mentors, fieldsIsActive } = this.state;
@@ -72,29 +68,28 @@ class App extends Component {
       <div className="app">
         <header className="main-header">
           <a className="logo" href="/">
-            <Logo
-              width={110}
-              height={50}
-              color="#68d5b1" />
-            <span>CODING COACH ALPHA</span>
+            <Logo width={110} height={50} color="#68d5b1" />
           </a>
-          <div className="social">
-            <Button role="link" circular size="mini" as="a" icon="github" color="black" href="https://github.com/Coding-Coach/find-a-mentor" target="_blank" />
-            <Button role="link" circular size="mini" as="a" icon="twitter" color="twitter" href="https://twitter.com/codingcoach_io" target="_blank" />
-            <Button role="link" circular size="mini" as="a" icon="facebook" color="facebook" href="https://www.facebook.com/codingcoachio/" target="_blank" />
-            <Button role="link" circular size="mini" as="a" icon="slack" color="purple" href="https://coding-coach.slack.com/join/shared_invite/enQtNTE2NDY4NTczNzE0LTMyOTAyZTFiYjE4OTUzYjgwYzk5MzlmYjgwNjUyNDZlZGY3NGVhYmU1NjdmZDQ3MmQ3YjRhYjJkMjM4OTYwNDA" target="_blank" />
-          </div>
+          <SocialLinks />
         </header>
         <div className="filters-outer">
           <div className="filters">
-            <Header as='h1'>
+            <Header as="h1">
               <div>
                 Find a mentor
-                <Button size="huge" floated='right' className="tertiary mobile" icon onClick={this.toggleFields}>
+                <Button
+                  size="huge"
+                  floated="right"
+                  className="tertiary mobile"
+                  icon
+                  onClick={this.toggleFields}
+                >
                   <Icon name="filter" />
                 </Button>
               </div>
-              <Header.Subheader>{mentors.length} mentors available</Header.Subheader>
+              <Header.Subheader>
+                {mentors.length} mentors available
+              </Header.Subheader>
             </Header>
             <div className="fields">
               <AutoComplete
@@ -114,7 +109,7 @@ class App extends Component {
         </div>
         <MentorsList
           className={classNames({
-            'active': fieldsIsActive
+            active: fieldsIsActive
           })}
           mentors={mentorsInList}
         />
