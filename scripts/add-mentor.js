9cb9f0b222e7d475f87b23f993ccbb69cb34ffc4
Gerson Niño <meta.author@gersonnino.me>
2019-03-14 14:49:28 -0500

    Separate branch and push process and refactor code

--- scripts/add-mentor.js
+++ scripts/add-mentor.js
@@ -15,7 +15,10 @@ const spinnerPull = new Ora({
 });
 const spinnerMerge = new Ora({
   text: 'Adding to mentors list'
-})
+});
+const spinnerBranch = new Ora({
+  text: 'Creating branch'
+});
 const spinnerCommit = new Ora({
   text: 'Adding to Git'
 });
@@ -27,73 +30,90 @@ async function gitPull() {
   await execa('git', ['pull']);
 }
 
+async function gitBranch(name) {
+  await execa('git', ['checkout','-b', `add-${name}-as-mentor`]);
+}
+
+async function gitCommit(name) {
+  await execa('git', ['add', '.']);
+  await execa('git', ['commit', '-m', `add ${name} as mentor`]);
+}
+
+async function gitPush(name) {
+  await execa('git', ['push', '--set-upstream', 'origin', `add-${name}-as-mentor`]);
+}
+
+const gitFunctions = {
+  pullProcess: async () => {
+    spinnerPull.start();
+    try {
+      await gitPull();
+      spinnerPull.succeed()
+    } catch (error) {
+      spinnerPull.fail();
+      console.error('Error: ', error.message);
+      return false;
+    }
+  },
+  branchProcess: async() => {
+    spinnerBranch.start();
+    try {
+      await gitBranch(answers.name);
+      spinnerBranch.succeed();
+    } catch (error) {
+      spinnerBranch.fail();
+      console.error('Error: ', error.message);
+      return false;
+    }
+  },
+  commitProcess: async() => {
+    spinnerCommit.start();
+    try {
+      await gitCommit(answers.name);
+      spinnerCommit.succeed();
+    } catch (error) {
+      spinnerCommit.fail();
+      console.error('Error: ', error.message);
+      return false;
+    }
+  },
+  pushProcess: async() => {
+    spinnerPush.start();
+    try {
+      await gitPush(answers.name.toLowerCase().replace(/\s/g, '-'));
+      spinnerPush.succeed();
+    } catch (error) {
+      spinnerPush.fail();
+      console.error('Error: ', error.message);
+      return false;
+    }
+  }
+};
+
 async function addToMentorsList(mentor) {
+  spinnerMerge.start();
   try {
     const text = await readFileAsync(absolutePath, {encoding: 'utf8'});
     const data = JSON.parse(text);
     data.push(mentor);
     const mentors = JSON.stringify(data, null, 2) + '\n';
     await writeFileAsync(absolutePath, mentors);
-    return true;
-  } catch (error) {
-    throw new Error(error);
-  }
-}
-
-async function gitCommit(name) {
-  const commitMessage = `add ${name} as mentor`
-  await execa('git', ['add', '.']);
-  await execa('git', ['commit', '-m', commitMessage]);
-}
-
-async function gitPush(name) {
-  const branchName = `add-${name}-as-mentor`
-  await execa('git', ['checkout','-b', branchName]);
-  await execa('git', ['push', '--set-upstream', 'origin', branchName]);
-}
-
-(async () => {
-  spinnerPull.start();
-  try {
-    await gitPull();
-    spinnerPull.succeed()
-  } catch (error) {
-    spinnerPull.fail();
-    console.error('Error: ', error.message);
-    return false;
-  }
-
-  const answers = await createUser();
-
-  spinnerMerge.start();
-  try {
-    await addToMentorsList(answers.name);
-    spinnerMerge.succeed()
+    spinnerMerge.succeed();
   } catch (error) {
     spinnerMerge.fail();
     console.error('Error: ', error.message);
     return false;
   }
+}
 
-  spinnerCommit.start();
-  try {
-    await gitCommit(answers.name);
-    spinnerCommit.succeed();
-  } catch (error) {
-    spinnerCommit.fail();
-    console.error('Error: ', error.message);
-    return false;
-  }
+(async () => {
 
-  spinnerPush.start();
-  try {
-    await gitPush(answers.name.toLowerCase().replace(/\s/g, '-'));
-    spinnerPush.succeed();
-  } catch (error) {
-    spinnerPush.fail();
-    console.error('Error: ', error.message);
-    return false;
-  }
+  await gitFunctions.pullProcess();
+  const answers = await createUser();
+  await addToMentorsList(answers);
+  await gitFunctions.branchProcess();
+  await gitFunctions.commitProcess();
+  await gitFunctions.pushProcess();
 
-  console.log('Mentor added. Please now do a git push and create a PR for finish the process. Thanks!');
+  console.log('\nMentor added.Please now create a PR for finish the process. Thanks!');
 })()
