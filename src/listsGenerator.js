6da6a88da9f079b0f036a44e5bd0ab9ecb9dc578
Cristian <cristian.echeverri4@gmail.com>
2019-03-19 12:59:19 +0100

    Filter mentors by name

--- src/listsGenerator.js
+++ src/listsGenerator.js
@@ -2,15 +2,18 @@ export function generateLists(mentors) {
   const json = {
     tags: [],
     countries: [],
+    names: []
   }
 
   for (let i = 0; i < mentors.length; i++) {
     json.tags.push(...(mentors[i].tags || []));
     json.countries.push(mentors[i].country);
+    json.names.push(mentors[i].name);
   }
 
   json.tags = [...new Set(json.tags.map(tag => tag.toLowerCase()))]
   json.countries = [...new Set(json.countries)]
+  json.names = [...new Set(json.names)]
 
   return json;
 }
