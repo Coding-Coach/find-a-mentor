e66ff7b03e2f21bfc6cfa3e95c5502389c7f3e83
Andrew Miracle <8960757+koolamusic@users.noreply.github.com>
2019-04-01 20:28:13 +0100

    fix typo in add mentor script (#232)

--- scripts/add-mentor.js
+++ scripts/add-mentor.js
@@ -11,7 +11,7 @@ const Ora = require('ora');
 const createUser = require('./create-user');
 
 const spinnerPull = new Ora({
-  text: 'Getting the last changes',
+  text: 'Retrieving most recent changes',
 });
 const spinnerMerge = new Ora({
   text: 'Adding to mentors list',
@@ -110,7 +110,7 @@ async function addToMentorsList(mentor) {
     spinnerMerge.fail();
     console.error('\x1b[31m%s\x1b[0m', 'Error: ' + error.message);
     throw new Error(
-      'There is a problem with the addition to the mentor list process'
+      'There seems to be a problem adding you to the mentors list process'
     );
   }
 }
@@ -124,13 +124,13 @@ async function addToMentorsList(mentor) {
     await gitFunctions.commitProcess(answers);
     await gitFunctions.pushProcess(answers);
     console.log(
-      '\nMentor added.Please now create a PR for finish the process. Thanks!'
+      '\nMentor added. Please create a PR to finish the process of adding a new mentor. Thanks!'
     );
   } catch (error) {
     console.log('\x1b[31m%s\x1b[0m', error.message);
     console.log(
       '\x1b[36m%s\x1b[0m',
-      'If you know what is the issue about please open an Issue or Pull Request.\nYou still can use the manual process for being added as mentor'
+      'If you know what this issue is about please open an Issue or Pull Request.\nHowever, you can also use the manual process to add yourself as a mentor'
     );
     process.exit();
   }
