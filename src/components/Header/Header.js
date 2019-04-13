b8ed3f6e8a5fd294acf9be5e416bb745a4150d3c
Andrew Miracle <me@andrewmiracle.com>
2019-04-13 12:52:46 +0100

    cleanup navigation menu for mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,31 +2,48 @@ import React, { Component, PureComponent } from 'react';
 import OffCanvas from 'react-aria-offcanvas';
 import './Header.css';
 
-
 class Navigation extends PureComponent {
-  render () {
+  openModal = (title, content, onClose) => {
+    this.setState({
+      modal: {
+        title,
+        content,
+        onClose,
+      },
+    });
+  };
+  render() {
     return (
       <nav id="menu" className="header-nav">
-    <ul>
-      <li>
-        <a href="https://mentors.codingcoach.io/">Home</a>
-      </li>
-      <li>
-        <a href="https://codingcoach.io/">About</a>
-      </li>
-      <li>
-        <a href="https://docs.google.com/document/d/1zKCxmIh0Sd4aWLiQncICOGm6uf38S0kJ0xb0qErNFVA/edit?usp=sharing">How it works</a>
-      </li>
-      <li>
-        <a href="https://github.com/Coding-Coach/find-a-mentor">Become a Mentor</a>
-      </li>
-    </ul>
-  </nav>
+        <ul className="header-nav__menu">
+          <li>
+            <a href="https://mentors.codingcoach.io/">Home</a>
+          </li>
+          <li>
+            <a href="https://codingcoach.io/">About</a>
+          </li>
+          <li>
+            <a href="https://docs.google.com/document/d/1zKCxmIh0Sd4aWLiQncICOGm6uf38S0kJ0xb0qErNFVA/edit?usp=sharing">
+              How it works
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Coding-Coach/find-a-mentor">
+              Become a Mentor
+            </a>
+          </li>
+        </ul>
 
-    )
+        <ul className="header-nav__modal">
+          <li>Cookies Policy</li>
+          <li>Code of Conduct</li>
+          <li>Terms & Conditions</li>
+          <li>Privacy Statement</li>
+        </ul>
+      </nav>
+    );
   }
 }
-
 
 export default class Header extends Component {
   state = {
@@ -44,14 +61,17 @@ export default class Header extends Component {
   render() {
     return (
       <>
-      <i  class="fa fa-bars" 
+        <i
+          className="fa fa-bars header-nav__open"
           aria-hidden="true"
           id="menu-button"
           aria-label="Menu"
           aria-controls="menu"
           aria-expanded={this.state.isOpen}
-          onClick={this._handleOpen}> Menu
-      </i>
+          onClick={this._handleOpen}
+        >
+          {' '}
+        </i>
 
         <OffCanvas
           isOpen={this.state.isOpen}
@@ -61,12 +81,12 @@ export default class Header extends Component {
           height="100vh"
           className="header-offcanvas"
         >
-
-      <i  class="fa fa-times" 
-          aria-hidden="true"
-          onClick={this._handleClose}
-          aria-label="Close"
-      ></i>
+          <i
+            className="fa fa-times header-nav__close"
+            aria-hidden="true"
+            onClick={this._handleClose}
+            aria-label="Close"
+          />
 
           <Navigation />
         </OffCanvas>
