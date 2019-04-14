098a52eba0ee3e1ccf5c537d8e0dbf6e14fff713
Mosh Feu <moshfeu@gmail.com>
2019-04-14 23:32:02 +0300

    clear the code a bit

--- src/components/Modal/ModalContent.js
+++ src/components/Modal/ModalContent.js
@@ -2,28 +2,18 @@ import React, { Component } from 'react';
 import Content from '../Content/Content';
 
 export default class ModalContent extends Component {
-    state = {
-      modal: {
-        title: null,
-        content: null,
-        onClose: null,
-      },
-    };
-
+  state = {
+    modal: {
+      title: null,
+      content: null,
+      onClose: null,
+    },
+  };
 
   render() {
     const { content, onClose, policyTitle, handleModal } = this.props
-    console.log("SEE STATE", this.state)
     return (
       <div>
-
-      {/* <div>
-      <Modal onClose={this.closeModal} title={modal.title}>
-        {modal.content}
-      </Modal>
-      </div> */}
-
-
         <li onClick={() => {
           this.setState({
             modal: {
@@ -37,42 +27,6 @@ export default class ModalContent extends Component {
         }}>
         {policyTitle}
         </li>
-      
-
-{/* 
-        <li onClick={() => this.openModal('Cookies Policy', <Content topic="cookies-policy" />)}>
-          Cookies Policy
-        </li>
-
-        <li onClick={() =>
-            this.openModal(
-              'Code of Conduct',
-              <Content topic="code-conduct" />
-            )
-          }
-        >
-          Code of Conduct
-    </li>
-        <li
-          onClick={() =>
-            this.openModal(
-              'Terms & Conditions',
-              <Content topic="terms-conditions" />
-            )
-          }
-        >
-          Terms & Conditions
-    </li>
-        <li
-          onClick={() =>
-            this.openModal(
-              'Privacy Statement',
-              <Content topic="privacy-policy" />
-            )
-          }
-        >
-          Privacy Statement
-    </li> */}
       </div>
     )
   }
