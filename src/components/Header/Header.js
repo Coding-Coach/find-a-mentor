b5484df00463bf5d367e56cd8b654152809097db
Andrew Miracle <me@andrewmiracle.com>
2019-04-14 02:55:23 +0100

    remove menu text in mobile menu icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -128,7 +128,6 @@ export default class Header extends Component {
             aria-expanded={isOpen}
             onClick={this._handleOpen}
           >
-          Menu
           </i>
         )}
 
