e2d8fb9d755868791ef150146193b467c8aedd34
Brent M Clark <brentmclark@gmail.com>
2019-05-14 23:35:21 -0500

    switch to getByTestId instead of selector syntax

--- cypress/integration/mentors.js
+++ cypress/integration/mentors.js
@@ -3,46 +3,46 @@ const { cy } = global;
 describe('Mentor Filtering', () => {
   it('can filter by technology', () => {
     cy.visit('/')
-      .get('[data-testid=name-filter-autocomplete]')
+      .getByTestId('name-filter-autocomplete')
       .type('Brent M Clark')
       .type('{enter}')
-      .get('[data-testid=technology-filter-autocomplete]')
+      .getByTestId('technology-filter-autocomplete')
       .type('reactjs')
       .type('{enter}')
-      .get('[data-testid=mentors-wrapper] .mentors-cards')
+      .get('.mentors-cards')
       .should('have.length', 1);
   });
 
   it('can filter by country', () => {
     cy.visit('/')
-      .get('[data-testid=name-filter-autocomplete]')
+      .getByTestId('name-filter-autocomplete')
       .type('Brent M Clark')
       .type('{enter}')
-      .get('[data-testid=country-filter-autocomplete]')
+      .getByTestId('country-filter-autocomplete')
       .type('United States')
       .type('{enter}')
-      .get('[data-testid=mentors-wrapper] .mentors-cards')
+      .get('.mentors-cards')
       .should('have.length', 1);
   });
 
   it('can filter by name', () => {
     cy.visit('/')
-      .get('[data-testid=name-filter-autocomplete]')
+      .getByTestId('name-filter-autocomplete')
       .type('Brent M Clark')
       .type('{enter}')
-      .get('[data-testid=mentors-wrapper] .mentors-cards')
+      .get('.mentors-cards')
       .should('have.length', 1);
   });
 
   it('can filter by language', () => {
     cy.visit('/')
-      .get('[data-testid=name-filter-autocomplete]')
+      .getByTestId('name-filter-autocomplete')
       .type('Brent M Clark')
       .type('{enter}')
-      .get('[data-testid=language-filter-autocomplete]')
+      .getByTestId('language-filter-autocomplete')
       .type('English')
       .type('{enter}')
-      .get('[data-testid=mentors-wrapper] .mentors-cards')
+      .get('.mentors-cards')
       .should('have.length', 1);
   });
 });
