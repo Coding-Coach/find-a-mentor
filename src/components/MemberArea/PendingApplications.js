00a19a3e4f4f5ec7a9ddb8f493cb7ed4fa8d1dc0
Mosh Feu <moshfeu@gmail.com>
2019-07-17 23:56:33 +0300

    add avatar and decline ui

--- src/components/MemberArea/PendingApplications.js
+++ src/components/MemberArea/PendingApplications.js
@@ -18,20 +18,31 @@ export default class PendingApplications extends Component {
     alert('done');
   }
 
+  decline = async application => {
+    // await approveApplication(application);
+    alert('decline');
+  }
+
   render() {
     const { applications } = this.state;
     return <table>
       <thead>
         <tr>
+          <th></th>
           <th>Name</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
       {
         applications.map(application =>
           <tr key={application._id}>
-            <td>{application.name}</td>
-            <td><button onClick={this.approve.bind(null, application)}>Approve</button></td>
+            <td><img alt={application.user.name} src={application.user.avatar} /></td>
+            <td>{application.user.name}</td>
+            <td>
+              <button onClick={this.approve.bind(null, application)}>Approve</button>
+              <button onClick={this.decline.bind(null, application)}>Decline</button>
+            </td>
           </tr>
         )
       }
