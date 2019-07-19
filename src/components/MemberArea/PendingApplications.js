f0851fa9a4f23a54a9c15212cb039c1960ba5644
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2019-07-19 10:44:45 +0200

    chore(PendingApplications): refactor export

--- src/components/MemberArea/PendingApplications.js
+++ src/components/MemberArea/PendingApplications.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { getPendingApplications, approveApplication } from '../../api';
 
-export default class PendingApplications extends Component {
+class PendingApplications extends Component {
   state = {
     applications: [],
   };
@@ -21,37 +21,35 @@ export default class PendingApplications extends Component {
   decline = async application => {
     // await approveApplication(application);
     alert('decline');
-  }
+  };
 
   render() {
     const { applications } = this.state;
-    return <table>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Name</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-      {
-        applications.map(application =>
-          <tr key={application._id}>
-            <td><img alt={application.user.name} src={application.user.avatar} /></td>
-            <td>{application.user.name}</td>
-            <td>
-              <button onClick={this.approve.bind(null, application)}>Approve</button>
-              <button onClick={this.decline.bind(null, application)}>Decline</button>
-            </td>
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th />
+            <th>Name</th>
+            <th />
           </tr>
         </thead>
         <tbody>
           {applications.map(application => (
             <tr key={application._id}>
-              <td>{application.name}</td>
+              <td>
+                <img
+                  alt={application.user.name}
+                  src={application.user.avatar}
+                />
+              </td>
+              <td>{application.user.name}</td>
               <td>
                 <button onClick={this.approve.bind(null, application)}>
                   Approve
+                </button>
+                <button onClick={this.decline.bind(null, application)}>
+                  Decline
                 </button>
               </td>
             </tr>
@@ -61,3 +59,5 @@ export default class PendingApplications extends Component {
     );
   }
 }
+
+export default PendingApplications;
