40fd4929cf17bb56d648b30dfd9b507403aeb47b
Wuilliam Lacruz <wuilliam321@gmail.com>
2019-07-29 18:38:06 -0300

    Update src/components/FilterClear/FilterClear.js
    Co-Authored-By: Mosh Feu <moshfeu@gmail.com>

--- src/components/FilterClear/FilterClear.js
+++ src/components/FilterClear/FilterClear.js
@@ -4,7 +4,7 @@ import './FilterClear.css';
 const FilterClear = ({ onClear }) => {
   return (
     <>
-      <span className="clear" data-testid="clear-filter" onClick={onClear}>
+      <button className="clear" data-testid="clear-filter" onClick={onClear}>
         clear
       </span>
     </>
