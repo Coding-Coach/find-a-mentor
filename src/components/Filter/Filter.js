bea1ba7b5f741024bf6ef4fe0e1a4966b3b50edb
Arun Kumar Mohan <arunmohandm@gmail.com>
2019-08-15 23:11:34 -0500

    Refactor Filter component using hooks

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,6 +1,6 @@
 import './Filter.css';
 
-import React, { Component } from 'react';
+import React, { useReducer } from 'react';
 import classNames from 'classnames';
 import AutoComplete from '../AutoComplete/AutoComplete';
 import Input from '../Input/Input';
@@ -8,106 +8,109 @@ import Switch from '../Switch/Switch';
 
 import { generateLists } from '../../listsGenerator';
 
-export default class Filter extends Component {
-  state = {
-    tag: '',
-    country: '',
-    name: '',
-    language: '',
-    showFilters: false,
+const Filter = props => {
+  const [state, setState] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      tag: '',
+      country: '',
+      name: '',
+      language: '',
+      showFilters: false,
+    }
+  );
+
+  const onTagSelect = tag => {
+    setState({ tag });
+    props.onTagSelected(tag);
   };
 
-  onTagSelect = tag => {
-    this.setState({ tag });
-    this.props.onTagSelected(tag);
+  const onCountrySelect = country => {
+    setState({ country });
+    props.onCountrySelected(country);
   };
 
-  onCountrySelect = country => {
-    this.setState({ country });
-    this.props.onCountrySelected(country);
+  const onNameSelect = name => {
+    setState({ name });
+    props.onNameSelected(name);
   };
 
-  onNameSelect = name => {
-    this.setState({ name });
-    this.props.onNameSelected(name);
+  const onLanguageSelect = language => {
+    setState({ language });
+    props.onLanguageSelected(language);
   };
 
-  onLanguageSelect = language => {
-    this.setState({ language });
-    this.props.onLanguageSelected(language);
-  };
-
-  onToggleFilter = () => {
-    this.setState({
-      showFilters: !this.state.showFilters,
+  const onToggleFilter = () => {
+    setState({
+      showFilters: !state.showFilters,
     });
-    this.props.onToggleFilter();
+    props.onToggleFilter();
   };
 
-  render() {
-    const { onToggleSwitch, clickedTag, clickedCountry, mentors } = this.props;
-    const { showFilters } = this.state;
-    const { tags, countries, names, languages } = generateLists(mentors);
+  const { onToggleSwitch, clickedTag, clickedCountry, mentors } = props;
+  const { showFilters } = state;
+  const { tags, countries, names, languages } = generateLists(mentors);
 
-    return (
-      <section aria-labelledby="filter" className="filter-wrapper">
-        <h3 id="filter">
-          Filter <span id="mentorCount">{this.props.mentorCount} Mentors</span>
-          <button
-            className="toggle-filter"
-            onClick={this.onToggleFilter}
-            aria-label="Toggle filter"
-          >
-            <i
-              className={classNames([
-                'fa fa-angle-down',
-                { 'show-filters': showFilters },
-              ])}
-            />
-          </button>
-        </h3>
-        <div className="inputs" aria-expanded={showFilters}>
-          <Input id="technology" label="Technology" key="technology">
-            <AutoComplete
-              id="technology"
-              source={tags}
-              onSelect={this.onTagSelect}
-              clickedTag={clickedTag}
-              showClear
-              data-testid="technology-filter-autocomplete"
-            />
-          </Input>
-          <Input id="country" label="Country" key="country">
-            <AutoComplete
-              id="country"
-              source={countries}
-              onSelect={this.onCountrySelect}
-              clickedCountry={clickedCountry}
-              showClear
-              data-testid="country-filter-autocomplete"
-            />
-          </Input>
-          <Input id="name" label="Name" key="name">
-            <AutoComplete
-              id="name"
-              source={names}
-              onSelect={this.onNameSelect}
-              showClear
-              data-testid="name-filter-autocomplete"
-            />
-          </Input>
-          <Input id="language" label="Language" key="language">
-            <AutoComplete
-              id="language"
-              source={languages}
-              onSelect={this.onLanguageSelect}
-              showClear
-              data-testid="language-filter-autocomplete"
-            />
-          </Input>
-          <Switch id="fav" label="My Favorites" onToggle={onToggleSwitch} />
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section aria-labelledby="filter" className="filter-wrapper">
+      <h3 id="filter">
+        Filter <span id="mentorCount">{props.mentorCount} Mentors</span>
+        <button
+          className="toggle-filter"
+          onClick={onToggleFilter}
+          aria-label="Toggle filter"
+        >
+          <i
+            className={classNames([
+              'fa fa-angle-down',
+              { 'show-filters': showFilters },
+            ])}
+          />
+        </button>
+      </h3>
+      <div className="inputs" aria-expanded={showFilters}>
+        <Input id="technology" label="Technology" key="technology">
+          <AutoComplete
+            id="technology"
+            source={tags}
+            onSelect={onTagSelect}
+            clickedTag={clickedTag}
+            showClear
+            data-testid="technology-filter-autocomplete"
+          />
+        </Input>
+        <Input id="country" label="Country" key="country">
+          <AutoComplete
+            id="country"
+            source={countries}
+            onSelect={onCountrySelect}
+            clickedCountry={clickedCountry}
+            showClear
+            data-testid="country-filter-autocomplete"
+          />
+        </Input>
+        <Input id="name" label="Name" key="name">
+          <AutoComplete
+            id="name"
+            source={names}
+            onSelect={onNameSelect}
+            showClear
+            data-testid="name-filter-autocomplete"
+          />
+        </Input>
+        <Input id="language" label="Language" key="language">
+          <AutoComplete
+            id="language"
+            source={languages}
+            onSelect={onLanguageSelect}
+            showClear
+            data-testid="language-filter-autocomplete"
+          />
+        </Input>
+        <Switch id="fav" label="My Favorites" onToggle={onToggleSwitch} />
+      </div>
+    </section>
+  );
+};
+
+export default Filter;
