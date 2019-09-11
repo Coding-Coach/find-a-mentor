dd70492e565514c7ad15574a63baa6a394c4076e
Brent <brent.clark@sportshubtech.com>
2019-09-10 21:31:24 -0500

    make prettier

--- cypress/integration/mentors.js
+++ cypress/integration/mentors.js
@@ -6,8 +6,8 @@ describe('Mentor Filtering', () => {
       .filterByName('Brent M Clark')
       .getByTestId('technology-filter-autocomplete')
       .type('reactjs{enter}', { force: true });
-      // .getByTestId('mentor-card')
-      // .should('have.length', 1);
+    // .getByTestId('mentor-card')
+    // .should('have.length', 1);
   });
 
   it('can filter by country', () => {
@@ -15,15 +15,14 @@ describe('Mentor Filtering', () => {
       .filterByName('Brent M Clark')
       .getByTestId('country-filter-autocomplete')
       .type('United States{enter}', { force: true });
-      // .getByTestId('mentor-card')
-      // .should('have.length', 1);
+    // .getByTestId('mentor-card')
+    // .should('have.length', 1);
   });
 
   it('can filter by name', () => {
-    cy.visit('/')
-      .filterByName('Brent M Clark');
-      // .getByTestId('mentor-card')
-      // .should('have.length', 1);
+    cy.visit('/').filterByName('Brent M Clark');
+    // .getByTestId('mentor-card')
+    // .should('have.length', 1);
   });
 
   it('can filter by language', () => {
@@ -31,16 +30,15 @@ describe('Mentor Filtering', () => {
       .filterByName('Brent M Clark')
       .getByTestId('language-filter-autocomplete')
       .type('English{enter}', { force: true });
-      // .getByTestId('mentor-card')
-      // .should('have.length', 1);
+    // .getByTestId('mentor-card')
+    // .should('have.length', 1);
   });
 
   it('can clear filter', () => {
     cy.visit('/')
       .filterByName('Brent M Clark')
       .getByTestId('clear-filter')
-      .click()
-    cy.getByTestId('name-filter-autocomplete')
-      .should('have.value', '')
+      .click();
+    cy.getByTestId('name-filter-autocomplete').should('have.value', '');
   });
 });
