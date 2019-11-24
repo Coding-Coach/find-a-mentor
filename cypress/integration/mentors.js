b443f34ee2dbb6d6237249c90d8e82a38f0c3627
Junip Dewan <junipd2@gmail.com>
2019-11-25 00:32:12 +0530

    added login and mentors fixutures in before hook

--- cypress/integration/mentors.js
+++ cypress/integration/mentors.js
@@ -1,16 +1,21 @@
 const { cy } = global;
 
 describe('Mentor Filtering', () => {
+  before(function(){
+    cy.login();
+    cy.server({urlMatchingOptions: { matchBase: false, dot: true }});
+    cy.route('GET', '/users/current', 'fixture:users/current/get');
+    cy.route('GET', '/mentors?limit=*', 'fixture:mentors/get');
+    cy.visit('/');
+  })
   it('can filter by technology', () => {
-    cy.visit('/')
-      .filterByName('Brent M Clark')
+    cy.filterByName('Brent M Clark')
       .getByTestId('technology-filter-autocomplete')
       .type('reactjs{enter}', { force: true });
   });
 
   it('can filter by country', () => {
-    cy.visit('/')
-      .filterByName('Brent M Clark')
+    cy.filterByName('Brent M Clark')
       .getByTestId('country-filter-autocomplete')
       .type('United States{enter}', { force: true });
     // .getByTestId('mentor-card')
@@ -18,14 +23,13 @@ describe('Mentor Filtering', () => {
   });
 
   it('can filter by name', () => {
-    cy.visit('/').filterByName('Brent M Clark');
+    cy.filterByName('Brent M Clark');
     // .getByTestId('mentor-card')
     // .should('have.length', 1);
   });
 
   it('can filter by language', () => {
-    cy.visit('/')
-      .filterByName('Brent M Clark')
+    cy.filterByName('Brent M Clark')
       .getByTestId('language-filter-autocomplete')
       .type('English{enter}', { force: true });
     // .getByTestId('mentor-card')
@@ -33,16 +37,13 @@ describe('Mentor Filtering', () => {
   });
 
   it('can clear filter', () => {
-    cy.visit('/')
-      .filterByName('Brent M Clark')
-      .getByTestId('clear-filter')
-      .click();
+    cy.filterByName('Brent M Clark')
+    cy.get('div.input-container:nth-child(3) div.clear-btn button').click();
     cy.getByTestId('name-filter-autocomplete').should('have.value', '');
   });
 
   it( 'verify filtered technology label on mentors card', ()=> {
-    cy.visit('/')
-      .getByTestId('technology-filter-autocomplete')
+    cy.getByTestId('technology-filter-autocomplete')
       .type('reactjs')
       .type('{enter}');
     cy.getAllByTestId('mentor-card').first()
@@ -50,20 +51,15 @@ describe('Mentor Filtering', () => {
   });
 
   it( 'verify filtered country label on mentors card', ()=> {
-    cy.visit('/')
-      .getByTestId('country-filter-autocomplete')
+    cy.getByTestId('country-filter-autocomplete')
       .type('United States')
       .type('{enter}');
     cy.getAllByTestId('mentor-card').first()
       .get('.country').contains('US')
   })
 
-  it('logged users can click on mentors channel', () => {
-    cy.login();
-    cy.server({urlMatchingOptions: { matchBase: false, dot: true }});
-    cy.route('GET', '/users/current', 'fixture:users/current/get');
-    cy.route('GET', '/mentors?limit=*', 'fixture:mentors/get');
-    cy.visit('/').filterByName('Brent M Clark')
+  it('logged users can click on mentors channel', () => { 
+    cy.filterByName('Brent M Clark')
     cy.getAllByTestId('mentor-card').first()
       .get('div.channels')
       .first()
