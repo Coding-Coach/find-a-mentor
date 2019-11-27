97def44dfe3e79213d5499f42ec89828f7254fd6
Mohit Tater <mohittater.iiita@gmail.com>
2019-11-27 15:47:08 +0530

    #661: Current favorite mentor list migration from local storage to server (#662)

--- src/favoriteManager.js
+++ src/favoriteManager.js
@@ -1,4 +1,6 @@
 import { getFavorites, addMentorToFavorites } from './api';
+import { report } from './ga';
+const LOCAL_FAV_KEY = "favs";
 
 export function toggle(mentor, favs) {
   const favIndex = favs.indexOf(mentor._id);
@@ -14,4 +16,29 @@ export function toggle(mentor, favs) {
 
 export function get() {
   return getFavorites();
+}
+
+export function readFavMentorsFromLocalStorage() {
+  let favMentorsFromLocalStorage = [];
+  const favsFromLocal = window.localStorage.getItem(LOCAL_FAV_KEY);
+  if (favsFromLocal) {
+    favMentorsFromLocalStorage = JSON.parse(favsFromLocal);
+    window.localStorage.removeItem(LOCAL_FAV_KEY);
+    report('Favorite', 'sync');
+  }
+  return favMentorsFromLocalStorage;
+}
+
+export function updateFavMentorsForUser(mentors) {
+    let timeOut = 0;
+    mentors.forEach(mentorId => {
+      setTimeout(
+        mentorId => {
+          addMentorToFavorites(mentorId);
+        },
+        timeOut,
+        mentorId
+      );
+      timeOut += 300;
+    });
 }
