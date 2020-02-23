077a0ef65ec73f8eaa52976dc5d0575ad78a1b9c
Mosh Feu <moshfeu@gmail.com>
2020-02-24 00:54:12 +0200

    replace react-body-backgroundcolor with code

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -3,30 +3,28 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import auth from '../../utils/auth';
 import Header from './Header/Header';
 import Navbar from './Navigation/Navbar';
-import BodyBackgroundColor from 'react-body-backgroundcolor';
+import './Me.css';
 
 const Me = ({ match: { url } }) => {
   const authenticated = auth.isAuthenticated();
 
   return (
-    <BodyBackgroundColor backgroundColor="#E0E0E0">
-      <div>
-        {authenticated ? (
-          <>
-            <Switch>
-              <Route
-                path={`${url}/home`}
-                render={() => <h1> hello from home </h1>}
-              />
-            </Switch>
-            <Navbar />
-            <Header />
-          </>
-        ) : (
-          <Redirect to="/" />
-        )}
-      </div>
-    </BodyBackgroundColor>
+    <div className="me-container">
+      {authenticated ? (
+        <>
+          <Switch>
+            <Route
+              path={`${url}/home`}
+              render={() => <h1> hello from home </h1>}
+            />
+          </Switch>
+          <Navbar />
+          <Header />
+        </>
+      ) : (
+        <Redirect to="/" />
+      )}
+    </div>
   );
 };
 
