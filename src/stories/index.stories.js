83dd21c61fa2cb1030ead164ced28e1f34eb92de
Mosh Feu <moshfeu@gmail.com>
2020-02-25 23:22:09 +0200

    shot I

---
+++ src/stories/index.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@storybook/react/demo';
+
+export default { title: 'Button' };
+
+export const withText = () => <Button>Hello Button</Button>;
+
+export const withEmoji = () => (
+  <Button>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+);
