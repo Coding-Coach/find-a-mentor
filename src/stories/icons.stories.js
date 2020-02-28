533a9ef36b9aafd5d813a5a2820d78313be45ac9
Mosh Feu <moshfeu@gmail.com>
2020-02-28 07:55:42 +0200

    use styled-components

--- src/stories/icons.stories.js
+++ src/stories/icons.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from './icons.module.scss';
-import classNames from 'classnames';
+import styled from 'styled-components';
 import { ReactComponent as Logo } from '../../assets/me/logo.svg';
 import { ReactComponent as Home } from '../../assets/me/home.svg';
 import { ReactComponent as Mentors } from '../../assets/me/mentors.svg';
@@ -11,15 +10,35 @@ const Icon = ({
   children,
   light = false
 }) => (
-  <div className={classNames([styles.icon, {[styles.dark]: light}])}>
+  <IconContainer light={light}>
     {children}
-  </div>
+  </IconContainer>
 )
 
 export const me = () => (
-  <div className={styles.icons}>
+  <Icons>
     <Icon light><Logo /></Icon>
     <Icon><Home /></Icon>
     <Icon><Mentors /></Icon>
-  </div>
+  </Icons>
 )
+
+const Icons = styled.div`
+  display: flex;
+`;
+
+const IconContainer = styled.div`
+  width: 44px;
+  margin: 5px;
+  padding: 5px;
+  display: flex;
+  border-radius: 5px;
+  align-items: center;
+  border: 1px solid #aaa;
+
+  ${props => props.light && {background: '#eee'}}
+
+  > svg {
+    width: 100%
+  }
+`;
