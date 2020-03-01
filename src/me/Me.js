a9ccac5245dac43ba81ca67e17ca5b7d46b5837f
Mosh Feu <moshfeu@gmail.com>
2020-03-01 22:33:58 +0200

    improve Me's layout

--- src/me/Me.js
+++ src/me/Me.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import styled from 'styled-components/macro';
 import auth from '../utils/auth';
+import Main from './Main';
 import Header from './Header/Header';
 import Navbar from './Navigation/Navbar';
-import './Me.css';
+import Home from './Routes/Home';
+import { desktop } from './styles/shared/devices';
 
 const Me = ({ match: { url } }) => {
   const authenticated = auth.isAuthenticated();
 
   return (
-    <div className="me-container">
+    <Container>
       {authenticated ? (
         <>
-          <Switch>
-            <Route
-              path={`${url}/home`}
-              render={() => <h1> hello from home </h1>}
-            />
-          </Switch>
           <Navbar />
           <Header />
+          <Main>
+            <Switch>
+              <Route path={`${url}/home`}>
+                <Home />
+              </Route>
+            </Switch>
+          </Main>
         </>
       ) : (
         <Redirect to="/" />
       )}
-    </div>
+    </Container>
   );
 };
 
 export default Me;
+
+const Container = styled.div`
+  min-height: 100vh;
+  background-color: #e0e0e0;
+
+  @media ${desktop} {
+    padding-left: 75px;
+  }
+`;
