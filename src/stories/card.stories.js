5ade795d6312be32ab3e7a740d4b2ed2a4dc09e1
Andre Cortes <andre.crts@gmail.com>
2020-03-06 23:45:29 -0600

    Change story to use real component

--- src/stories/card.stories.js
+++ src/stories/card.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { action } from '@storybook/addon-actions';
+import Card from '../Me/components/Card';
 
 export default { title: 'Card' };
 
@@ -9,36 +9,10 @@ const CardContainer = styled.div`
   padding: 15px;
 `;
 
-const Card = styled.div`
-  background-color: #ffffff;
-  -webkit-box-shadow: 4px 4px 9px -3px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 4px 4px 9px -3px rgba(0, 0, 0, 0.75);
-  box-shadow: 4px 4px 9px -3px rgba(0, 0, 0, 0.75);
-`;
-
-const Header = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 14px 20px 14px;
-`;
-
-const EditButton = styled.button`
-  background-color: transparent;
-  border: none;
-  text-decoration: underline;
-  color: #4f4f4f;
-`;
-const Edit = ({ onEdit }) => {
-  return onEdit ? <EditButton onClick={onEdit}>Edit</EditButton> : null;
-};
+const action = () => console.log('Clicked');
 
 export const me = () => (
   <CardContainer>
-    <Card>
-      <Header>
-        <h2>Mentor Profile</h2>
-        <Edit onEdit={action('clicked')} />
-      </Header>
-    </Card>
+    <Card title="Mentor Profile" onEdit={action} />
   </CardContainer>
 );
