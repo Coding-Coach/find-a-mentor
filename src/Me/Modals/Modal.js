aacb9c7db266ac8f61c4efc6ae595cfeba3fc59b
Angelika Jarosz <angelikajarosz@gmail.com>
2020-03-25 15:01:29 -0400

    Me Modal component

--- src/Me/Modals/Modal.js
+++ src/Me/Modals/Modal.js
@@ -1,8 +1,126 @@
 import React from 'react';
+import styled from 'styled-components';
+import { desktop } from '../styles/shared/devices';
 
-export const Modal = ({closeModal}) => (
-  <div>
-    Modal!
-    <button onClick={closeModal}>X</button>
-  </div>
+const ModalContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  top: 0;
+  left: 0;
+  position: fixed;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CloseIconButton = styled.button`
+  background-color: transparent;
+  color: #000;
+  font-weight: 500;
+  font-size: 20px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 5px 10px;
+  width: 28px;
+  height: 28px;
+`;
+
+const ContentContainer = styled.div`
+  height: 80%;
+  width: 100%;
+  margin: 0 auto;
+  overflow-y: auto;
+  padding: 20px;
+
+  @media ${desktop} {
+    height: 70%;
+  }
+`;
+
+const Title = styled.div`
+  width: 100%;
+  color: #4F4F4F;
+  font-family: Lato;
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 34px;
+  text-align: center;
+  margin: 50px 0;
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  height: 20%;
+  width: 100%;
+  bottom: 0;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media ${desktop} {
+    height: 30%;
+  }
+`;
+
+const ButtonBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media ${desktop} {
+    flex-direction: row;
+  }
+`;
+
+const Button = styled.button`
+  height: 30px;
+  width: 285px;
+  border-radius: 3px;
+  font-family: Lato;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: center;
+  margin-bottom: 1rem;
+
+  @media ${desktop} {
+    height: 30px;
+    width: 151px;
+    margin: 1rem;
+  }
+`;
+
+const PrimaryButton = styled(Button)`
+  background-color: #69D5B1;
+  color: #FFF;
+  font-family: Lato;
+  font-size: 14px;
+  order: -1;
+`;
+
+const SecondaryButton = styled(Button)`
+  box-sizing: border-box;
+  background-color: #FFF;
+  border: 2px solid #69D5B1;
+  color: #69D5B1;
+`;
+
+export const Modal = ({onClose, onSave, title, children }) => (
+  <ModalContainer>
+    <CloseIconButton  onClick={onClose}>x</CloseIconButton>
+    <ContentContainer>
+      <Title>
+        {title || null}
+      </Title>
+      {children}
+    </ContentContainer>
+    <Footer>
+      <ButtonBar>
+        <SecondaryButton onClick={onClose}>Close</SecondaryButton>
+        <PrimaryButton onClick={onSave}>Save</PrimaryButton>
+      </ButtonBar>
+    </Footer>
+  </ModalContainer>
 )
