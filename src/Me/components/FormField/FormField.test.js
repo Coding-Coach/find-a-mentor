02ab07d965737936170a4cdd681c352813a7729e
luca.campli <campli.luca@gmail.com>
2020-04-10 13:58:18 -0300

    add tests

---
+++ src/Me/components/FormField/FormField.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FormField from './';
+import { render } from '@testing-library/react';
+import { formFieldContext } from './formContext';
+
+describe('FormField component', () => {
+  it('renders properly', () => {
+    const { container } = render(<FormField label="test label" />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  it('renders the label when it is defined', () => {
+    const { getByText } = render(<FormField label="test label" />);
+    expect(getByText('test label')).toBeInTheDocument();
+  });
+  it('passes down the id', () => {
+    let formFieldId;
+    render(
+      <FormField>
+        <formFieldContext.Consumer>
+          {id => (formFieldId = id)}
+        </formFieldContext.Consumer>
+      </FormField>
+    );
+    expect(formFieldId).toMatch('form-field-');
+  });
+});
