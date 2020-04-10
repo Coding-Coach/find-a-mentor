066ca936b62f26d3a345f3731852a6f66587f196
luca.campli <campli.luca@gmail.com>
2020-04-10 13:58:11 -0300

    add stories

---
+++ src/stories/button.stories.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import 'styled-components/macro';
+import Button from '../Me/components/Button';
+import '../index.css';
+import { StoriesContainer } from './StoriesContainer';
+
+export default { title: 'Button' };
+
+export const Buttons = () => {
+  const [msg, setMsg] = useState('');
+  return (
+    <StoriesContainer>
+      <h1>Update Profile</h1>
+      <Button onClick={() => setMsg('Primary clicked!')}>Primary</Button>
+      <Button skin="secondary" onClick={() => setMsg('Secondary clicked!')}>
+        Secondary
+      </Button>
+      <h2>{msg}</h2>
+    </StoriesContainer>
+  );
+};
---
+++ src/stories/select.stories.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import 'styled-components/macro';
+import Select from '../Me/components/Select';
+import FormField from '../Me/components/FormField';
+import '../index.css';
+import { StoriesContainer } from './StoriesContainer';
+
+export default { title: 'Select' };
+const options = [
+  { value: 1, label: 'one' },
+  { value: 2, label: 'two' },
+  { value: 3, label: 'three' },
+];
+
+export const SingleSelect = () => {
+  const [selectedValues, setSelectedValues] = useState();
+
+  return (
+    <StoriesContainer>
+      <FormField label="Single select" css={{ width: '60%' }}>
+        <Select
+          value={selectedValues}
+          onChange={selected => {
+            setSelectedValues(selected);
+          }}
+          options={options}
+          placeholder="Select a number"
+        />
+      </FormField>
+    </StoriesContainer>
+  );
+};
+
+export const MultiSelect = () => {
+  const [selectedValues, setSelectedValues] = useState();
+
+  return (
+    <StoriesContainer>
+      <FormField label="Multi select" css={{ width: '60%' }}>
+        <Select
+          isMulti={true}
+          value={selectedValues}
+          onChange={selected => {
+            setSelectedValues(selected);
+          }}
+          options={options}
+          placeholder="Select a number"
+        />
+      </FormField>
+    </StoriesContainer>
+  );
+};
+
+export const MultiSelectWithMaxItems = () => {
+  const [selectedValues, setSelectedValues] = useState();
+
+  return (
+    <StoriesContainer>
+      <FormField label="Multi select" css={{ width: '60%' }}>
+        <Select
+          isMulti={true}
+          value={selectedValues}
+          onChange={selected => {
+            setSelectedValues(selected);
+          }}
+          options={options}
+          maxSelections={2}
+          placeholder="Select a number"
+        />
+      </FormField>
+    </StoriesContainer>
+  );
+};
