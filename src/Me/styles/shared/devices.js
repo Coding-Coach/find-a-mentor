b1d8402d6c87aee2f3fd623f7b865057c83da563
Mosh Feu <moshfeu@gmail.com>
2020-04-16 00:29:37 +0300

    clean unnecessary code

--- src/Me/styles/shared/devices.js
+++ src/Me/styles/shared/devices.js
@@ -1,18 +1,4 @@
-import {css} from 'styled-components/macro';
-
 const desktop = `(min-width: 801px)`;
 const mobile = `(max-width: 800px)`;
 
-const desktopHidden = css`
-  @media ${desktop} {
-    display: none;
-  }
-`;
-
-const mobileHidden = css`
-  @media ${mobile} {
-    display: none;
-  }
-`;
-
-export { desktop, mobile, desktopHidden, mobileHidden };
+export { desktop, mobile };
