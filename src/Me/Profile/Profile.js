46a6371abbf09403a4b5f5dd4df4f3bee49e1c7c
Mooli <moolipit@gmail.com>
2020-12-15 09:14:50 +0000

    Refactor/profile (#740)
    * feature: add List and ListItem
    
    * refactor: Profile
    
    * fix: pass tags to List.item as array
    
    * modifiy changes according to PR review

--- src/Me/Profile/Profile.js
+++ src/Me/Profile/Profile.js
@@ -1,57 +1,18 @@
+import ISO6391 from 'iso-639-1';
 import React, { useContext } from 'react';
 import { toast } from 'react-toastify';
-import styled from 'styled-components';
-
-import UserContext from '../../context/userContext/UserContext';
-import { useModal } from '../../context/modalContext/ModalContext';
-import Card from '../components/Card';
-import EditMentorDetails from '../Modals/EditMentorDetails';
-import {
-  updateMentor,
-  getCurrentUser,
-  clearCurrentUser,
-} from '../../api/index';
-import messages from '../../messages';
-import ISO6391 from 'iso-639-1';
 import countries from 'svg-country-flags/countries.json';
-
-import { ReactComponent as EmailIcon } from '../../assets/me/icon-email.svg';
-import { ReactComponent as SpokenLanguagesIcon } from '../../assets/me/icon-spokenLanguages.svg';
-import { ReactComponent as CountryIcon } from '../../assets/me/icon-country.svg';
-import { ReactComponent as TitleIcon } from '../../assets/me/icon-title.svg';
-import { ReactComponent as TagsIcon } from '../../assets/me/icon-tags.svg';
-import { ReactComponent as AvailableIcon } from '../../assets/me/icon-available.svg';
-import { ReactComponent as UnavailableIcon } from '../../assets/me/icon-unavailable.svg';
-import { ReactComponent as DescriptionIcon } from '../../assets/me/icon-description.svg';
-
-// Object w/ keyed SVG Components
-const Components = {
-  email: EmailIcon,
-  spokenLanguages: SpokenLanguagesIcon,
-  country: CountryIcon,
-  title: TitleIcon,
-  tags: TagsIcon,
-  available: AvailableIcon,
-  unavailable: UnavailableIcon,
-  description: DescriptionIcon,
-};
-
-// Styled Components
-const ProfileRow = styled.div`
-  display: grid;
-  grid-template-columns: 40px auto;
-  margin-top: 5px;
-`;
-
-const ProfileIcon = styled.div`
-  width: 24px;
-  height: 100%;
-  margin-right: 20px;
-`;
-
-const ProfileText = styled.div`
-  padding-top: 4px;
-`;
+import {
+  clearCurrentUser,
+  getCurrentUser,
+  updateMentor,
+} from '../../api/index';
+import { useModal } from '../../context/modalContext/ModalContext';
+import UserContext from '../../context/userContext/UserContext';
+import messages from '../../messages';
+import Card from '../components/Card';
+import List from '../components/List';
+import EditMentorDetails from '../Modals/EditMentorDetails';
 
 // UPDATE THIS ACTION FOR EDIT PROFILE LINK ONCE EDIT PROFILE COMPONENT IS CREATED //
 const handleUpdateMentor = async (updatedUserInfo, updateUser, closeModal) => {
@@ -71,55 +32,6 @@ const handleUpdateMentor = async (updatedUserInfo, updateUser, closeModal) => {
   }
 };
 
-//--- Container for Profile Data ---//
-const ProfileData = props => {
-  const profileLines = Object.entries(props).map(([type, lineVal]) => (
-    <ProfileLine type={type} val={lineVal} key={type} />
-  ));
-  return <div>{profileLines}</div>;
-};
-
-//--- Profile Line Items - SVG & Text ---//
-const ProfileLine = props => {
-  // variables for svg component and text values
-  let TagName = Components[props.type];
-  let lineText = '';
-
-  // join array properties, and set availability svg to check or x
-  switch (props.type) {
-    case 'spokenLanguages':
-      var languagesSpoken = [];
-      props.val.map(language =>
-        languagesSpoken.push(ISO6391.getName(language))
-      );
-      lineText = languagesSpoken.join(', ');
-      break;
-    case 'tags':
-      lineText = props.val.join(', ');
-      break;
-    case 'available':
-      TagName =
-        props.val === true ? Components.available : Components.unavailable;
-      lineText = props.val === true ? 'available' : 'unavailable';
-      break;
-    case 'country':
-      lineText = countries[props.val];
-      break;
-    default:
-      lineText = props.val;
-      break;
-  }
-
-  return (
-    <ProfileRow>
-      <ProfileIcon>
-        <TagName />
-      </ProfileIcon>
-      <ProfileText>{lineText}</ProfileText>
-    </ProfileRow>
-  );
-};
-
 //--- Profile Component ---//
 const Profile = () => {
   // get user from context
@@ -135,17 +47,23 @@ const Profile = () => {
   return (
     <Card title="My Profile" onEdit={openModal}>
       {currentUser && (
-        <>
-          <ProfileData
-            email={currentUser.email}
-            spokenLanguages={currentUser.spokenLanguages}
-            country={currentUser.country}
-            title={currentUser.title}
-            tags={currentUser.tags}
-            available={currentUser.available}
-            description={currentUser.description}
+        <List>
+          <List.Item type="email" value={currentUser.email} />
+          <List.Item
+            type="spokenLanguages"
+            value={currentUser.spokenLanguages
+              .map(language => ISO6391.getName(language))
+              .join(', ')}
           />
-        </>
+          <List.Item type="country" value={countries[currentUser.country]} />
+          <List.Item type="title" value={currentUser.title} />
+          <List.Item type="tags" value={currentUser.tags.join(', ')} />
+          <List.Item
+            type="available"
+            value={currentUser.available ? 'available' : 'unavailable'}
+          />
+          <List.Item type="description" value={currentUser.description} />
+        </List>
       )}
     </Card>
   );
---
+++ src/Me/components/List/ListItem.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as AvailableIcon } from '../../../assets/me/icon-available.svg';
+import { ReactComponent as CountryIcon } from '../../../assets/me/icon-country.svg';
+import { ReactComponent as DescriptionIcon } from '../../../assets/me/icon-description.svg';
+import { ReactComponent as EmailIcon } from '../../../assets/me/icon-email.svg';
+import { ReactComponent as SpokenLanguagesIcon } from '../../../assets/me/icon-spokenLanguages.svg';
+import { ReactComponent as TagsIcon } from '../../../assets/me/icon-tags.svg';
+import { ReactComponent as TitleIcon } from '../../../assets/me/icon-title.svg';
+import { ReactComponent as UnavailableIcon } from '../../../assets/me/icon-unavailable.svg';
+
+// Object w/ keyed SVG Components
+const icons = {
+  email: EmailIcon,
+  spokenLanguages: SpokenLanguagesIcon,
+  country: CountryIcon,
+  title: TitleIcon,
+  tags: TagsIcon,
+  available: AvailableIcon,
+  unavailable: UnavailableIcon,
+  description: DescriptionIcon,
+};
+
+// Styled Components
+const ItemRow = styled.div`
+  display: grid;
+  grid-template-columns: 40px auto;
+  margin-top: 5px;
+`;
+
+const ItemIcon = styled.div`
+  width: 24px;
+  height: 100%;
+  margin-right: 20px;
+`;
+
+const ItemText = styled.div`
+  padding-top: 4px;
+`;
+
+const ListItem = ({ type, value }) => {
+  const Icon = icons[type];
+  return (
+    <ItemRow>
+      {Icon && (
+        <ItemIcon data-testid={type}>
+          <Icon />
+        </ItemIcon>
+      )}
+      <ItemText>{value}</ItemText>
+    </ItemRow>
+  );
+};
+
+ListItem.propTypes = {
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  type: PropTypes.oneOf([
+    'email',
+    'spokenLanguages',
+    'country',
+    'title',
+    'tags',
+    'available',
+    'unavailable',
+    'description',
+  ]),
+};
+
+export default ListItem;
