f9c1f07a596295ba4faa7578575edcb6798070d2
Mooli m <moolipit@gmail.com>
2020-12-31 22:22:44 +0000

    add tests

--- src/Me/components/RichList/RichList.test.js
+++ src/Me/components/RichList/RichList.test.js
@@ -5,35 +5,34 @@ import RichList from '.';
 describe('RichList component', () => {
   const items = [
     {
+      id: '123',
       avatar: '../../assets/me/mentors.svg',
       title: 'Mooli m',
       subtitle: 'web dev',
       tag: {
         value: 'pending',
-        theme: 'secondary'
+        theme: 'secondary',
       },
-      info: '3 days ago'
+      info: '3 days ago',
     },
     {
+      id: '456',
       avatar: '../../assets/me/camera.svg',
       title: 'Someone',
-      subtitle: 'Rockstar dev',
+      subtitle: 'Rock star dev',
       tag: {
         value: 'Accepted',
-        theme: 'primary'
+        theme: 'primary',
       },
-      info: '6 days ago'
+      info: '6 days ago',
     },
   ];
 
   it('Should render 2 items', async () => {
-    const { findAllByText } = render(
-      <RichList items={items} />
-    );
-    const rgx = new RegExp(`${items[0].title}|${items[1].title}`)
-    const listTtems = await findAllByText(rgx)
+    const { findAllByText } = render(<RichList items={items} />);
+    const rgx = new RegExp(`${items[0].title}|${items[1].title}`);
+    const itemsEl = await findAllByText(rgx);
 
-    expect(listTtems.length).toBe(2);
+    expect(itemsEl.length).toBe(2);
   });
-
 });
