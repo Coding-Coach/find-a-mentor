78e35c71f4a5eee8d8dddc36a5674185d3856004
Mooli m <moolipit@gmail.com>
2021-01-02 23:15:08 +0000

    add test to MentorshipReq and RichList

--- src/Me/components/RichList/RichList.test.js
+++ src/Me/components/RichList/RichList.test.js
@@ -1,38 +1,54 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import RichList from '.';
 
-describe('RichList component', () => {
-  const items = [
-    {
-      id: '123',
-      avatar: '../../assets/me/mentors.svg',
-      title: 'Mooli m',
-      subtitle: 'web dev',
-      tag: {
-        value: 'pending',
-        theme: 'secondary',
-      },
-      info: '3 days ago',
+const items = [
+  {
+    id: '123',
+    avatar: '../../assets/me/mentors.svg',
+    title: 'Mooli m',
+    subtitle: 'web dev',
+    tag: {
+      value: 'pending',
+      theme: 'secondary',
     },
-    {
-      id: '456',
-      avatar: '../../assets/me/camera.svg',
-      title: 'Someone',
-      subtitle: 'Rock star dev',
-      tag: {
-        value: 'Accepted',
-        theme: 'primary',
-      },
-      info: '6 days ago',
+    info: '3 days ago',
+    children: <h1>123</h1>,
+  },
+  {
+    id: '456',
+    avatar: '../../assets/me/camera.svg',
+    title: 'Someone',
+    subtitle: 'Rock star dev',
+    tag: {
+      value: 'Accepted',
+      theme: 'primary',
     },
-  ];
+    info: '6 days ago',
+    children: <h1>456</h1>,
+  },
+];
 
+describe('RichList component', () => {
   it('Should render 2 items', async () => {
     const { findAllByText } = render(<RichList items={items} />);
     const rgx = new RegExp(`${items[0].title}|${items[1].title}`);
     const itemsEl = await findAllByText(rgx);
 
     expect(itemsEl.length).toBe(2);
+  });
+
+  it('Should show/hide children content on item click', () => {
+    const { id, title } = items[0];
+    const { getByText, queryByText } = render(<RichList items={items} />);
+    const titleEl = getByText(title);
+
+    fireEvent.click(titleEl);
+
+    getByText(id);
+
+    fireEvent.click(titleEl);
+
+    expect(queryByText(id)).toBeFalsy();
   });
 });
