82692616fae7c43553d02dcc03bf1167cde3888d
Mooli m <moolipit@gmail.com>
2021-01-03 22:47:21 +0000

    add loader aria support

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SrOnly = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+`;
 
 export function Loader() {
-  return <i className="loader fa fa-spinner fa-spin" />;
+  return (
+    <i className="loader fa fa-spinner fa-spin" role="status">
+      <SrOnly>Loading...</SrOnly>
+    </i>
+  );
 }
