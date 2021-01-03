b84e09ec9fe00ef46d3cc41f5cc153e8b4258d48
Mooli m <moolipit@gmail.com>
2021-01-03 22:46:41 +0000

    fix: Mentorship test error

--- src/Me/MentorshipReq/MentorshipReq.js
+++ src/Me/MentorshipReq/MentorshipReq.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import { getMentorshipRequests, getCurrentUser } from '../../api';
 import RichList from '../components/RichList';
 import Card, { Content } from '../components/Card';
@@ -7,6 +7,8 @@ import UserContext from '../../context/userContext/UserContext';
 import RequestContent from './RequestContent';
 import { Loader } from '../../components/Loader';
 import { formatRequestTime } from '../../helpers/mentorship';
+import { toast } from 'react-toastify';
+import messages from '../../messages';
 
 const Root = styled.div`
   ${({ hasReq }) => hasReq && Content} {
@@ -26,41 +28,54 @@ const STATUS_THEME = {
   Cancelled: 'disabled',
 };
 
-const mapData = res =>
-  res.map(({ id, status, date, message, background, expectation, user }) => ({
-    id: id,
-    avatar: user.avatar,
-    title: user.name,
-    subtitle: user.title,
-    tag: {
-      value: status,
-      theme: STATUS_THEME[status],
-    },
-    info: formatRequestTime(date),
-    children: message && background && expectation && (
-      <RequestContent {...{ message, background, expectation }} />
-    ),
-  }));
-
 const MentorshipReq = () => {
   const [state, setState] = useState();
   const { currentUser, updateUser } = useContext(UserContext);
   const userId = currentUser?._id;
   const hasReq = currentUser?.mentorshipReq?.length > 0;
   const isLoading = !Array.isArray(currentUser?.mentorshipReq);
+  const isMount = useRef(true);
+
+  const mapData = res =>
+    res.map(({ id, status, date, message, background, expectation, user }) => ({
+      id: id,
+      avatar: user.avatar,
+      title: user.name,
+      subtitle: user.title,
+      tag: {
+        value: status,
+        theme: STATUS_THEME[status],
+      },
+      info: formatRequestTime(date),
+      children: message && background && expectation && (
+        <RequestContent
+          {...{ message, background, expectation }}
+          onClick={() => acceptReq(id)}
+        />
+      ),
+    }));
+
+  const acceptReq = id => {
+    toast.error(messages.GENERIC_ERROR);
+  };
 
   const setMentorshipReq = async () => {
     if (hasReq) {
       setState(mapData(currentUser?.mentorshipReq));
     } else {
       getMentorshipRequests(userId).then(({ data }) => {
-        updateUser({ ...currentUser, mentorshipReq: data });
-        setState(mapData(data));
+        if (isMount.current) {
+          updateUser({ ...currentUser, mentorshipReq: data });
+          setState(mapData(data));
+        }
       });
     }
   };
 
-  const init = () => getCurrentUser().then(user => updateUser(user));
+  const init = () =>
+    getCurrentUser().then(user => {
+      if (isMount.current) updateUser(user);
+    });
 
   useEffect(() => {
     if (userId) {
@@ -69,6 +84,13 @@ const MentorshipReq = () => {
       init();
     }
   }, [userId]);
+
+  useEffect(() => {
+    isMount.current = true;
+    return () => {
+      isMount.current = false;
+    };
+  }, []);
 
   const render = () => {
     if (isLoading)
--- src/Me/MentorshipReq/MentorshipReq.test.js
+++ src/Me/MentorshipReq/MentorshipReq.test.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import { fireEvent, render, reqData, userData } from './test-setup';
+import {
+  fireEvent,
+  act,
+  render,
+  cleanup,
+  reqData,
+  screen,
+  waitForElementToBeRemoved,
+  userData,
+} from './test-setup';
 import MentorshipReq from './MentorshipReq';
 import * as api from '../../api';
+
 describe('MentorshipReq', () => {
   jest.mock('../../api/index.js');
 
   beforeEach(() => {
     api.getCurrentUser = jest.fn(() => Promise.resolve(userData));
   });
+
+  afterEach(cleanup);
+
   it('Fetch and render a list of mentorship requests', async () => {
     api.getMentorshipRequests = jest.fn(() => Promise.resolve(reqData));
-    const { findAllByText, debug } = render(<MentorshipReq />);
+    const { findAllByText } = render(<MentorshipReq />);
     const reqEls = await findAllByText(/User.*/);
 
     expect(reqEls.length).toBe(3);
+  });
+  it('Shows appropriate message if there are no mentorship requests', async () => {
+    const promise = Promise.resolve({ ...reqData, data: [] });
+    api.getMentorshipRequests = jest.fn(() => promise);
+    const { getByText } = render(<MentorshipReq />);
+
+    await waitForElementToBeRemoved(() => document.querySelector('i.loader'));
+
+    //FIX: await waitForElementToBeRemoved(() => screen.getByRole('status'));
+
+    getByText(/No requests/);
+  });
+
+  it('Render Loading component while waiting for mentorship requests to load', async () => {
+    const promise = Promise.resolve(reqData);
+    api.getMentorshipRequests = jest.fn(() => promise);
+    const { getByRole } = render(<MentorshipReq />);
+    getByRole('status');
+
+    await act(() => promise);
   });
 });
