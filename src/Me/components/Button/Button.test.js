9490ff317466d3ceb88238ace858e5c2e8c612e2
Mooli Morano <mooli.morano@peluk.org>
2021-01-11 19:59:08 +0000

    add Button OnClick test

--- src/Me/components/Button/Button.test.js
+++ src/Me/components/Button/Button.test.js
@@ -1,8 +1,34 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Button from './';
 
 describe('Button component', () => {
+
+  it('passes down other props to the button tag', () => {
+    const { getByTestId } = render(
+      <Button data-testid="test-button" onClick={() => {}}>
+        click me
+      </Button>
+    );
+    expect(getByTestId('test-button')).toBeInTheDocument();
+  });
+
+  it(`fires 'prop.onClick' upon user click`, () => {
+    const onClick = jest.fn(() => {})
+    const { getByText } = render(
+      <Button onClick={onClick}>Click Me</Button>
+    );
+
+    const btn = getByText('Click Me')
+
+    fireEvent.click(btn)
+
+    expect(onClick).toHaveBeenCalledTimes(1)
+
+  })
+  
+});
+describe('Button theme', () => {
   it('renders primary button by default', () => {
     const { container } = render(
       <Button onClick={() => {}}>primary button</Button>
@@ -28,12 +54,4 @@ describe('Button component', () => {
     expect(container).toMatchSnapshot();
   });
 
-  it('passes down other props to the button tag', () => {
-    const { getByTestId } = render(
-      <Button data-testid="test-button" onClick={() => {}}>
-        click me
-      </Button>
-    );
-    expect(getByTestId('test-button')).toBeInTheDocument();
-  });
-});
+})
