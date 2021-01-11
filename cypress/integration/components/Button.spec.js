fa7692ea46a8f034550f9123032db8f93c8df09b
Brent M Clark <brentmclark@gmail.com>
2021-01-11 09:51:29 -0600

    prettier

--- cypress/integration/components/Button.spec.js
+++ cypress/integration/components/Button.spec.js
@@ -3,26 +3,20 @@ const { cy } = global;
 describe('Buttons', () => {
   it('renders the primary button', () => {
     cy.visit('localhost:6006/iframe.html?id=button--primary');
-    cy.get('#root')
-        .within(() => {
-            cy.get('button')
-            .should('have.text', 'Primary')
-        })
+    cy.get('#root').within(() => {
+      cy.get('button').should('have.text', 'Primary');
+    });
   });
   it('renders the secondary button', () => {
     cy.visit('localhost:6006/iframe.html?id=button--secondary');
-    cy.get('#root')
-        .within(() => {
-            cy.get('button')
-            .should('have.text', 'Secondary')
-        }) 
+    cy.get('#root').within(() => {
+      cy.get('button').should('have.text', 'Secondary');
+    });
   });
   it('renders the danger button', () => {
     cy.visit('localhost:6006/iframe.html?id=button--danger');
-    cy.get('#root')
-        .within(() => {
-            cy.get('button')
-            .should('have.text', 'Danger')
-        }) 
+    cy.get('#root').within(() => {
+      cy.get('button').should('have.text', 'Danger');
+    });
   });
 });
