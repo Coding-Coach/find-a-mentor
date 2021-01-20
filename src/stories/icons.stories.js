4b0517afc431a41c1a14e9b21a0ea423909297dd
Brent M Clark <brentmclark@gmail.com>
2021-01-19 22:38:23 -0600

    a quick note

--- src/stories/icons.stories.js
+++ src/stories/icons.stories.js
@@ -11,12 +11,16 @@ import { ReactComponent as SpokenLanguages } from '../assets/me/icon-spokenLangu
 import { ReactComponent as Tags } from '../assets/me/icon-tags.svg';
 import { ReactComponent as Title } from '../assets/me/icon-title.svg';
 import { ReactComponent as Unavailable } from '../assets/me/icon-unavailable.svg';
-
-
 import { ReactComponent as Logo } from '../assets/me/logo.svg';
 import { ReactComponent as Mentors } from '../assets/me/mentors.svg';
 
 export default { title: 'Icons' };
+
+/*
+NOTE: I really wanted this file to be an mdx file like the rest of them, but the loaders 
+and plugins involved don't work properly with SVGA and MDX.  I might take another swing
+at it later, but for now I settled for a working version even if it's not mdx.
+*/
 
 const Icon = ({ children, light = false }) => (
   <IconContainer light={light}>{children}</IconContainer>
