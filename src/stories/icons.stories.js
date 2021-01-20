d08e44ed102d64daa836367dff310a73dc9922e8
Brent Clark <brentmclark@gmail.com>
2021-01-19 23:51:01 -0600

    Add more icons to the Icon story (#766)
    * delete Icon stories
    
    * add all icons
    
    * a quick note

--- src/stories/icons.stories.js
+++ src/stories/icons.stories.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Logo } from '../assets/me/logo.svg';
+import { ReactComponent as Camera } from '../assets/me/camera.svg';
 import { ReactComponent as Home } from '../assets/me/home.svg';
+import { ReactComponent as Available } from '../assets/me/icon-available.svg';
+import { ReactComponent as Country } from '../assets/me/icon-country.svg';
+import { ReactComponent as Description } from '../assets/me/icon-description.svg';
+import { ReactComponent as DoorExit } from '../assets/me/icon-door-exit.svg';
+import { ReactComponent as Email } from '../assets/me/icon-email.svg';
+import { ReactComponent as SpokenLanguages } from '../assets/me/icon-spokenLanguages.svg';
+import { ReactComponent as Tags } from '../assets/me/icon-tags.svg';
+import { ReactComponent as Title } from '../assets/me/icon-title.svg';
+import { ReactComponent as Unavailable } from '../assets/me/icon-unavailable.svg';
+import { ReactComponent as Logo } from '../assets/me/logo.svg';
 import { ReactComponent as Mentors } from '../assets/me/mentors.svg';
 
 export default { title: 'Icons' };
+
+/*
+NOTE: I really wanted this file to be an mdx file like the rest of them, but the loaders 
+and plugins involved don't work properly with SVGA and MDX.  I might take another swing
+at it later, but for now I settled for a working version even if it's not mdx.
+*/
 
 const Icon = ({ children, light = false }) => (
   <IconContainer light={light}>{children}</IconContainer>
@@ -12,11 +28,41 @@ const Icon = ({ children, light = false }) => (
 
 export const me = () => (
   <Icons>
-    <Icon light>
-      <Logo />
+    <Icon>
+      <Camera />
     </Icon>
     <Icon>
       <Home />
+    </Icon>
+    <Icon>
+      <Available />
+    </Icon>
+    <Icon>
+      <Country />
+    </Icon>
+    <Icon>
+      <Description />
+    </Icon>
+    <Icon>
+      <DoorExit />
+    </Icon>
+    <Icon>
+      <Email />
+    </Icon>
+    <Icon>
+      <SpokenLanguages />
+    </Icon>
+    <Icon>
+      <Tags />
+    </Icon>
+    <Icon>
+      <Title />
+    </Icon>
+    <Icon>
+      <Unavailable />
+    </Icon>
+    <Icon light>
+      <Logo />
     </Icon>
     <Icon>
       <Mentors />
