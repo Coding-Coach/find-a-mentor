811c1b27eb44bd1e5ede20ab91ebd942033d86c2
Mosh Feu <moshfeu@gmail.com>
2021-02-11 13:58:42 +0200

    increase mentors - 1100

--- src/api/index.js
+++ src/api/index.js
@@ -102,7 +102,7 @@ export function clearCurrentUser() {
 
 export async function getMentors() {
   // TODO remove prepage: 1000 once the pagination will be ready
-  const res = await makeApiCall(`${paths.MENTORS}?limit=1000`);
+  const res = await makeApiCall(`${paths.MENTORS}?limit=1100`);
   if (res.data) {
     return shuffle(res.data);
   }
