be69f17fe7c7fe03f2853cc81589e8984cac2583
Mosh Feu <moshfeu@gmail.com>
2021-08-21 21:11:48 +0300

    clean and style

--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -1,6 +1,7 @@
 import notFoundImage from './assets/404.svg';
 import Header from './components/Header/Header';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const StyleImage = styled.div`
   padding: 20px;
   .imge {
@@ -20,6 +21,11 @@ const Content = styled.div`
     display: inline-grid;
     grid-template-columns: auto auto;
   }
+
+  p {
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+  }
 `;
 
 const PageNotFound = () => {
@@ -35,24 +41,20 @@ const PageNotFound = () => {
               </StyleImage>
             </div>
             <div className="col">
-              <div className="flex-1 md:pl-16 ">
-                <h1>Not Found</h1>
-                <h3>
-                  You just hit a route that doesn&#39;t exist... What can you do
-                  now? That's a good question! There are several things you can
-                  do, going to <a href="/">home page</a> would be a good idea.
-                  You might want to <a href="/blog">read the blog</a>, we have
-                  very interesting articles and tutorials! Maybe looking for{' '}
-                  <a href="https://mentors.codingcoach.io">a mentor</a> to
-                  improve your career?
-                </h3>
-                <h3>
-                  How did you get here? Is this a broken link within the blog?
-                  Or maybe a link from a third party website? Please{' '}
-                  <a href="mailto:admin@codingcoach.io">let us know</a> and we
-                  will fix it ASAP!
-                </h3>
-              </div>
+              <h1>Not Found</h1>
+              <p>
+                You just hit a route that doesn't exist... What can you do now?
+                That's a good question! There are several things you can do,
+                going to <a href="/">home page</a> would be a good idea. You
+                might want to <a href="/blog">read the blog</a>, we have very
+                interesting articles and tutorials! Maybe looking for{' '}
+                <Link to="/">a mentor</Link> to improve your career?
+              </p>
+              <p>
+                How did you get here? Is this a broken link out there? Please{' '}
+                <a href="mailto:admin@codingcoach.io">let us know</a> and we
+                will fix it ASAP!
+              </p>
             </div>
           </div>
         </div>
