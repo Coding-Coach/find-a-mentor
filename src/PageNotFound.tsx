41f475ea9a62af6cc45654586011a288b3dc8bce
Mosh Feu <moshfeu@gmail.com>
2021-08-28 00:38:45 +0300

    fix(404): text and mobile design (#835)

--- src/PageNotFound.tsx
+++ src/PageNotFound.tsx
@@ -2,24 +2,39 @@ import notFoundImage from './assets/404.svg';
 import Header from './components/Header/Header';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import { desktop, mobile } from './Me/styles/shared/devices';
+
 const StyleImage = styled.div`
-  padding: 20px;
+  @media ${desktop} {
+    padding: 20px;
+  }
+
   .imge {
     max-width: 35vw;
     max-height: 35vh;
   }
 `;
+
 const Content = styled.div`
   .container {
     padding-right: 15px;
     padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
-    padding-top: 15vh;
+
+    @media ${mobile} {
+      padding-top: 15px;
+    }
+
+    @media ${desktop} {
+      padding-top: 15vh;
+    }
   }
+
   .row {
-    display: inline-grid;
-    grid-template-columns: auto auto;
+    @media ${desktop} {
+      display: inline-grid;
+      grid-template-columns: auto auto;
+    }
   }
 
   p {
@@ -31,6 +46,9 @@ const Content = styled.div`
 const PageNotFound = () => {
   return (
     <>
+      <Helmet>
+        <title>Page not found | CodingCoach</title>
+      </Helmet>
       <Header />
       <Content>
         <div className="container py-16 blog-content md:flex md:py-64">
@@ -45,10 +63,10 @@ const PageNotFound = () => {
               <p>
                 You just hit a route that doesn't exist... What can you do now?
                 That's a good question! There are several things you can do,
-                going to <a href="/">home page</a> would be a good idea. You
-                might want to <a href="/blog">read the blog</a>, we have very
-                interesting articles and tutorials! Maybe looking for{' '}
-                <Link to="/">a mentor</Link> to improve your career?
+                going to <Link to="/">home page</Link> would be a good idea. You
+                might want to{' '}
+                <a href="https://codingcoach.io/blog">read the blog</a>, we have
+                very interesting articles and tutorials!
               </p>
               <p>
                 How did you get here? Is this a broken link out there? Please{' '}
