0542a2ded62bb29fe00ea70efe11c63d1e494819
Steve <stevenjchang@gmail.com>
2022-02-25 13:16:16 -0800

    fix: css vars not working (#889)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ import '../src/components/Input/Input.css';
 import '../src/components/MentorsList/MentorList.css';
 import '../src/components/Modal/Modal.css';
 import '../src/components/SocialLinks/SocialLinks.css';
+import '../src/index.css';
 
 // import App from 'next/app'
 
@@ -19,36 +20,36 @@ import { AuthProvider } from '../src/context/authContext/AuthContext';
 import { UserProvider } from '../src/context/userContext/UserContext';
 import { FiltersProvider } from '../src/context/filtersContext/FiltersContext';
 import { ModalHookProvider } from '../src/context/modalContext/ModalContext';
-import { MentorsProvider } from  '../src/context/mentorsContext/MentorsContext'
+import { MentorsProvider } from '../src/context/mentorsContext/MentorsContext';
 
 function MyApp({ Component, pageProps }) {
-    return (
-      <AuthProvider>
-        <ApiProvider>
-          <UserProvider>
-            <ModalHookProvider>
-              <FiltersProvider>
-                <MentorsProvider>
-                  <Component {...pageProps} />
-                </MentorsProvider>
-              </FiltersProvider>
-            </ModalHookProvider>
-          </UserProvider>
-        </ApiProvider>
-      </AuthProvider>
-    )
-  }
-  
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
-  
-  export default MyApp
+  return (
+    <AuthProvider>
+      <ApiProvider>
+        <UserProvider>
+          <ModalHookProvider>
+            <FiltersProvider>
+              <MentorsProvider>
+                <Component {...pageProps} />
+              </MentorsProvider>
+            </FiltersProvider>
+          </ModalHookProvider>
+        </UserProvider>
+      </ApiProvider>
+    </AuthProvider>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;
