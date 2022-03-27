6a90707180e178d7f2901334ba503027f5640ecd
Moshe Feuchtwanger <moshfeu@gmail.com>
2022-03-27 03:35:01 +0300

    format

--- src/helpers/getTitleTags.tsx
+++ src/helpers/getTitleTags.tsx
@@ -3,5 +3,5 @@ export const getTitleTags = (title: string) => {
     <title>{title}</title>,
     <meta property="og:title" content={title} key="ogtitle" />,
     <meta name="twitter:title" content={title} key="twittertitle" />,
-  ]
-}
+  ];
+};
