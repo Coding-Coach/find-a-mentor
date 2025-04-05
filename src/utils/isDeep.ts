5f6c77a060b5fa5deb9218883b4f6ec0eadbae4b
Mosh Feu <moshfeu@gmail.com>
2025-04-05 10:22:19 +0300

    Update isDeep.ts

--- src/utils/isDeep.ts
+++ src/utils/isDeep.ts
@@ -4,6 +4,5 @@ export const isDeep = () => {
   if (typeof window === 'undefined') {
     return false;
   }
-  console.log(11111, process.env.NEXT_PUBLIC_GREETING, new URLSearchParams(window.location.search).get('deep'))
-  return new URLSearchParams(window.location.search).get('deep') === process.env.NEXT_PUBLIC_GREETING;
+  return new URLSearchParams(window.location.search).get('deep') === 'deep';
 }
