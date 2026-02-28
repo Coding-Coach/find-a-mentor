f72efebd9ad13fd090e65df5cdd7512bd678c097
Mosh Feu <moshfeu@gmail.com>
2026-02-28 09:02:17 +0200

    chore(build) enable typecheck

--- next.config.js
+++ next.config.js
@@ -3,17 +3,10 @@
  */
 const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
     tsconfigPath: './tsconfig.json',
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    dirs: ['pages', 'src'],
   },
   webpack5: true,
   webpack: (config) => {
