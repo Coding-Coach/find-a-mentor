23d06448ff5697a0a992685e5983056503766ffc
Mosh Feu <moshfeu@gmail.com>
2026-02-28 08:48:00 +0200

    fix

--- src/Me/Routes/Home/Avatar/Avatar.tsx
+++ src/Me/Routes/Home/Avatar/Avatar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components/macro';
 import { useUser } from '../../../../context/userContext/UserContext';
 
